41b5ae2b72d09858611e69f581fa014bdeba46e8
Stanislav <stanislav.oliynyk@gmail.com>
2023-08-23 02:39:57 +0200

    delete-task

--- components/TodoList.js
+++ components/TodoList.js
@@ -1,37 +1,41 @@
-import  { useState } from 'react';
+import { useState } from 'react';
 
 function TodoList() {
-  const [todos, setTodos] = useState([]);  
-  const [newTodo, setNewTodo] = useState('');  
+  const [todos, setTodos] = useState([]);
+  const [newTodo, setNewTodo] = useState('');
 
-  function handleAddTodo() {
+  function addNewTodo() {
     if (newTodo.trim() !== '') {
-      
       setTodos([...todos, newTodo]);
       setNewTodo('');
     }
   }
 
+  function removeTodo(index) {
+    const newTodos = todos.filter((_, i) => i !== index);
+    setTodos(newTodos);
+  }
+
   return (
     <div>
       <h2>Tasks</h2>
-      
-      <input
-        type="text"
-        value={newTodo}
-        onChange={(e) => setNewTodo(e.target.value)}
-      />
-      
-      <button onClick={handleAddTodo}>add new Task</button>
-      
+      <input type="text" value={newTodo} onChange={(e) => setNewTodo(e.target.value)} />
+      <button onClick={addNewTodo}>Add</button>
       <ul>
         {todos.map((todo, index) => (
-          <li key={index}>{todo}</li>
+          <li key={index}>
+            {todo}
+            <button onClick={() => removeTodo(index)}>Delete</button>
+          </li>
         ))}
       </ul>
+       
+        <button onClick={() => setTodos([])}>Clear All</button>
+      
     </div>
   );
 }
 
 export default TodoList;
+
 
