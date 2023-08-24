1a47c23315d4a11c768ad884235377f4d231871a
Stanislav <stanislav.oliynyk@gmail.com>
2023-08-24 11:53:50 +0200

    update new StyledComponents

--- components/TodoList.js
+++ components/TodoList.js
@@ -1,4 +1,30 @@
-import { useState } from 'react';
+import React, { useState } from 'react';
+import styled from 'styled-components';
+import { FlexContainer, FlexChild } from './StyledComponents';
+
+
+const TodoContainer = styled.div`
+  flex: 1;
+`;
+
+const PriorityTitle = styled.h3`
+  font-size: 18px;
+  margin-bottom: 8px;
+`;
+
+const TodoItem = styled.li`
+  display: flex;
+  align-items: center;
+  margin-bottom: 4px;
+`;
+
+const TodoText = styled.span`
+  flex: 1;
+`;
+
+const TodoButton = styled.button`
+  margin-left: 8px;
+`;
 
 function TodoList() {
   const [todos, setTodos] = useState([]);
@@ -7,34 +33,20 @@ function TodoList() {
   const [editedTodoText, setEditedTodoText] = useState('');
   const [priority, setPriority] = useState('medium');
 
-  function addNewTodo() {
-    if (newTodo.trim() !== '') {
-      const todoItem = {
-        id: Math.random().toString(32).substring(2),
-        text: newTodo,
-        priority: priority,
-      };
-      setTodos([...todos, todoItem]);
-      setNewTodo('');
-    }
-  }
+  const handleNewTodoChange = (e) => {
+    setNewTodo(e.target.value);
+  };
 
-  function removeTodo(id) {
-    const newTodos = todos.filter((todo) => todo.id !== id);
-    setTodos(newTodos);
-  }
+  const handlePriorityChange = (e) => {
+    setPriority(e.target.value);
+  };
 
-  function startEditing(id, text) {
+  const handleTodoEditing = (id, text) => {
     setEditTodoId(id);
     setEditedTodoText(text);
-  }
+  };
 
-  function cancelEditing() {
-    setEditTodoId(null);
-    setEditedTodoText('');
-  }
-
-  function saveEditedTodo(id) {
+  const handleTodoSaving = (id) => {
     const updatedTodos = todos.map((todo) =>
       todo.id === id ? { ...todo, text: editedTodoText } : todo
     );
@@ -46,113 +58,77 @@ function TodoList() {
     setTodos(updatedTodos);
     setEditTodoId(null);
     setEditedTodoText('');
-  }
+  };
 
-  function handlePriorityChange(event) {
-    setPriority(event.target.value);
-  }
+  const handleCancelEditing = () => {
+    setEditTodoId(null);
+    setEditedTodoText('');
+  };
+
+  const handleAddNewTodo = () => {
+    if (newTodo.trim() !== '') {
+      const todoItem = {
+        id: Math.random().toString(32).substring(2),
+        text: newTodo,
+        priority: priority,
+      };
+      setTodos([...todos, todoItem]);
+      setNewTodo('');
+    }
+  };
+
+  const handleRemoveTodo = (id) => {
+    const newTodos = todos.filter((todo) => todo.id !== id);
+    setTodos(newTodos);
+  };
 
   const priorityGroups = {
-    high: todos.filter(todo => todo.priority === 'high'),
-    medium: todos.filter(todo => todo.priority === 'medium'),
-    low: todos.filter(todo => todo.priority === 'low'),
+    high: todos.filter((todo) => todo.priority === 'high'),
+    medium: todos.filter((todo) => todo.priority === 'medium'),
+    low: todos.filter((todo) => todo.priority === 'low'),
   };
 
   return (
     <div>
       <h2>Tasks</h2>
-      <input
-        type="text"
-        value={newTodo}
-        onChange={(e) => setNewTodo(e.target.value)}
-      />
+      <input type="text" value={newTodo} onChange={handleNewTodoChange} />
       <select value={priority} onChange={handlePriorityChange}>
         <option value="high">High Priority</option>
         <option value="medium">Medium Priority</option>
         <option value="low">Low Priority</option>
       </select>
-      <button onClick={addNewTodo}>Add</button>
+      <button onClick={handleAddNewTodo}>Add</button>
 
-      <div style={{ display: 'flex' }}>
-        <div style={{ flex: 1 }}>
-          <h3>High Priority</h3>
-          <ul>
-            {priorityGroups.high.map((todo) => (
-              <li key={todo.id}>
-                {editTodoId === todo.id ? (
-                  <>
-                    <input
-                      type="text"
-                      value={editedTodoText}
-                      onChange={(e) => setEditedTodoText(e.target.value)}
-                    />
-                    <button onClick={() => saveEditedTodo(todo.id)}>Save</button>
-                    <button onClick={cancelEditing}>Cancel</button>
-                  </>
-                ) : (
-                  <>
-                    {todo.text}
-                    <button onClick={() => removeTodo(todo.id)}>Delete</button>
-                    <button onClick={() => startEditing(todo.id, todo.text)}>Edit</button>
-                  </>
-                )}
-              </li>
-            ))}
-          </ul>
-        </div>
-        <div style={{ flex: 1 }}>
-          <h3>Medium Priority</h3>
-          <ul>
-            {priorityGroups.medium.map((todo) => (
-              <li key={todo.id}>
-                {editTodoId === todo.id ? (
-                  <>
-                    <input
-                      type="text"
-                      value={editedTodoText}
-                      onChange={(e) => setEditedTodoText(e.target.value)}
-                    />
-                    <button onClick={() => saveEditedTodo(todo.id)}>Save</button>
-                    <button onClick={cancelEditing}>Cancel</button>
-                  </>
-                ) : (
-                  <>
-                    {todo.text}
-                    <button onClick={() => removeTodo(todo.id)}>Delete</button>
-                    <button onClick={() => startEditing(todo.id, todo.text)}>Edit</button>
-                  </>
-                )}
-              </li>
-            ))}
-          </ul>
-        </div>
-        <div style={{ flex: 1 }}>
-          <h3>Low Priority</h3>
-          <ul>
-            {priorityGroups.low.map((todo) => (
-              <li key={todo.id}>
-                {editTodoId === todo.id ? (
-                  <>
-                    <input
-                      type="text"
-                      value={editedTodoText}
-                      onChange={(e) => setEditedTodoText(e.target.value)}
-                    />
-                    <button onClick={() => saveEditedTodo(todo.id)}>Save</button>
-                    <button onClick={cancelEditing}>Cancel</button>
-                  </>
-                ) : (
-                  <>
-                    {todo.text}
-                    <button onClick={() => removeTodo(todo.id)}>Delete</button>
-                    <button onClick={() => startEditing(todo.id, todo.text)}>Edit</button>
-                  </>
-                )}
-              </li>
-            ))}
-          </ul>
-        </div>
-      </div>
+      <FlexContainer>
+        {Object.keys(priorityGroups).map((priorityKey) => (
+          <TodoContainer key={priorityKey}>
+            <PriorityTitle>{priorityKey} Priority</PriorityTitle>
+            <ul>
+              {priorityGroups[priorityKey].map((todo) => (
+                <TodoItem key={todo.id}>
+                  {editTodoId === todo.id ? (
+                    <>
+                      <input
+                        type="text"
+                        value={editedTodoText}
+                        onChange={(e) => setEditedTodoText(e.target.value)}
+                      />
+                      <TodoButton onClick={() => handleTodoSaving(todo.id)}>Save</TodoButton>
+                      <TodoButton onClick={handleCancelEditing}>Cancel</TodoButton>
+                    </>
+                  ) : (
+                    <>
+                      <TodoText>{todo.text}</TodoText>
+                      <TodoButton onClick={() => handleRemoveTodo(todo.id)}>Delete</TodoButton>
+                      <TodoButton onClick={() => handleTodoEditing(todo.id, todo.text)}>Edit</TodoButton>
+                    </>
+                  )}
+                </TodoItem>
+              ))}
+            </ul>
+          </TodoContainer>
+        ))}
+      </FlexContainer>
 
       <button onClick={() => setTodos([])}>Clear All</button>
     </div>
--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,17 @@
 import React from 'react';
+import styled from 'styled-components'; 
 import TodoList from '../components/TodoList';
+
+
+const Title = styled.h1`
+  font-size: 24px;
+  color: #333;
+`;
 
 const HomePage = () => {
   return (
     <div>
-      <h1>My To-Do App</h1>
+      <Title>My To-Do App</Title>
       <TodoList />
     </div>
   );
