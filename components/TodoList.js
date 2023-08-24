80e13201b5f5f2d6e89a07d4f1d94caa4ec7c466
Stanislav <stanislav.oliynyk@gmail.com>
2023-08-24 07:32:26 +0200

    TaskSorting

--- components/TodoList.js
+++ components/TodoList.js
@@ -5,12 +5,14 @@ function TodoList() {
   const [newTodo, setNewTodo] = useState('');
   const [editTodoId, setEditTodoId] = useState(null);
   const [editedTodoText, setEditedTodoText] = useState('');
+  const [priority, setPriority] = useState('medium');
 
   function addNewTodo() {
     if (newTodo.trim() !== '') {
       const todoItem = {
         id: Math.random().toString(32).substring(2),
         text: newTodo,
+        priority: priority,
       };
       setTodos([...todos, todoItem]);
       setNewTodo('');
@@ -36,19 +38,25 @@ function TodoList() {
     const updatedTodos = todos.map((todo) =>
       todo.id === id ? { ...todo, text: editedTodoText } : todo
     );
-  
-  
-    if (editedTodoText.trim() === "") {
-      console.log();
-      
+
+    if (editedTodoText.trim() === '') {
       return;
     }
-  
+
     setTodos(updatedTodos);
     setEditTodoId(null);
     setEditedTodoText('');
-  
   }
+
+  function handlePriorityChange(event) {
+    setPriority(event.target.value);
+  }
+
+  const priorityGroups = {
+    high: todos.filter(todo => todo.priority === 'high'),
+    medium: todos.filter(todo => todo.priority === 'medium'),
+    low: todos.filter(todo => todo.priority === 'low'),
+  };
 
   return (
     <div>
@@ -58,32 +66,94 @@ function TodoList() {
         value={newTodo}
         onChange={(e) => setNewTodo(e.target.value)}
       />
+      <select value={priority} onChange={handlePriorityChange}>
+        <option value="high">High Priority</option>
+        <option value="medium">Medium Priority</option>
+        <option value="low">Low Priority</option>
+      </select>
       <button onClick={addNewTodo}>Add</button>
-      <ul>
-        {todos.map((todo) => {
-          if (editTodoId === todo.id) {
-            return (
+
+      <div style={{ display: 'flex' }}>
+        <div style={{ flex: 1 }}>
+          <h3>High Priority</h3>
+          <ul>
+            {priorityGroups.high.map((todo) => (
               <li key={todo.id}>
-                <input
-                  type="text"
-                  value={editedTodoText}
-                  onChange={(e) => setEditedTodoText(e.target.value)}
-                />
-                <button onClick={() => saveEditedTodo(todo.id)}>Save</button>
-                <button onClick={cancelEditing}>Cancel</button>
+                {editTodoId === todo.id ? (
+                  <>
+                    <input
+                      type="text"
+                      value={editedTodoText}
+                      onChange={(e) => setEditedTodoText(e.target.value)}
+                    />
+                    <button onClick={() => saveEditedTodo(todo.id)}>Save</button>
+                    <button onClick={cancelEditing}>Cancel</button>
+                  </>
+                ) : (
+                  <>
+                    {todo.text}
+                    <button onClick={() => removeTodo(todo.id)}>Delete</button>
+                    <button onClick={() => startEditing(todo.id, todo.text)}>Edit</button>
+                  </>
+                )}
               </li>
-            );
-          } else {
-            return (
+            ))}
+          </ul>
+        </div>
+        <div style={{ flex: 1 }}>
+          <h3>Medium Priority</h3>
+          <ul>
+            {priorityGroups.medium.map((todo) => (
               <li key={todo.id}>
-                {todo.text}
-                <button onClick={() => removeTodo(todo.id)}>Delete</button>
-                <button onClick={() => startEditing(todo.id, todo.text)}>Edit</button>
+                {editTodoId === todo.id ? (
+                  <>
+                    <input
+                      type="text"
+                      value={editedTodoText}
+                      onChange={(e) => setEditedTodoText(e.target.value)}
+                    />
+                    <button onClick={() => saveEditedTodo(todo.id)}>Save</button>
+                    <button onClick={cancelEditing}>Cancel</button>
+                  </>
+                ) : (
+                  <>
+                    {todo.text}
+                    <button onClick={() => removeTodo(todo.id)}>Delete</button>
+                    <button onClick={() => startEditing(todo.id, todo.text)}>Edit</button>
+                  </>
+                )}
               </li>
-            );
-          }
-        })}
-      </ul>
+            ))}
+          </ul>
+        </div>
+        <div style={{ flex: 1 }}>
+          <h3>Low Priority</h3>
+          <ul>
+            {priorityGroups.low.map((todo) => (
+              <li key={todo.id}>
+                {editTodoId === todo.id ? (
+                  <>
+                    <input
+                      type="text"
+                      value={editedTodoText}
+                      onChange={(e) => setEditedTodoText(e.target.value)}
+                    />
+                    <button onClick={() => saveEditedTodo(todo.id)}>Save</button>
+                    <button onClick={cancelEditing}>Cancel</button>
+                  </>
+                ) : (
+                  <>
+                    {todo.text}
+                    <button onClick={() => removeTodo(todo.id)}>Delete</button>
+                    <button onClick={() => startEditing(todo.id, todo.text)}>Edit</button>
+                  </>
+                )}
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
+
       <button onClick={() => setTodos([])}>Clear All</button>
     </div>
   );
